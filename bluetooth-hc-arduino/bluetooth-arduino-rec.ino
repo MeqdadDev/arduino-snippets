a7aebd60f9b2eae37b58890d2e48b9db65e8d6a1
MeqdadDev <meqdad.darweesh@gmail.com>
2024-01-26 16:25:07 +0200

    upload bluetooth hc-05 receiver for arduino uno

---
+++ bluetooth-hc-arduino/bluetooth-arduino-rec.ino
@@ -0,0 +1,23 @@
+const int ledPin = 13;  // Change this to the pin where your LED is connected
+const int buzzerPin = 12;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char receivedChar = Serial.read();
+    
+    if (receivedChar == '1') {
+      digitalWrite(ledPin, HIGH);  // Turn on the LED when '1' is received
+      digitalWrite(buzzerPin, HIGH);  // Turn on the Buzzer when '1' is received
+      delay(3000);  // Adjust the delay as needed
+      digitalWrite(ledPin, LOW);  // Turn off the LED
+      digitalWrite(buzzerPin, LOW);  // Turn off the Buzzer
+    }
+  }
+  delay(50);  // Adjust the delay as needed
+}
