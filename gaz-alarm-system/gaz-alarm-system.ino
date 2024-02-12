98a926d38638a3dfb790bf81293fff6c93dd1002
MeqdadDev <meqdad.darweesh@gmail.com>
2024-02-12 15:43:37 +0200

    update gaz alarm system using mq2: formatting file

--- gaz-alarm-system/gaz-alarm-system.ino
+++ gaz-alarm-system/gaz-alarm-system.ino
@@ -1,39 +1,50 @@
+/*
+Gaz/Smoke Alarm System
+Code for Sender.
+
+By: MeqdadDev
+*/
+
 #define MQ2pin 0
 #define greenLedPin 9
 #define redLedPin 13
 #define buzzerPin 7
 #define motorPin 4
 
-float sensorValue;  // variable to store sensor value
+float sensorValue; // variable to store sensor value
 
-void setup() {
-	Serial.begin(9600); // sets the serial port to 9600
-	Serial.println("Warming up!");
-  pinMode(redLedPin, OUTPUT);
-  pinMode(greenLedPin, OUTPUT);
-  pinMode(buzzerPin, OUTPUT);
-  pinMode(motorPin, OUTPUT);
-  pinMode(MQ2pin, INPUT);
-	delay(20000); // allow the MQ2 to warm up
+void setup()
+{
+    Serial.begin(9600); // sets the serial port to 9600
+    Serial.println("Warming up!");
+    pinMode(redLedPin, OUTPUT);
+    pinMode(greenLedPin, OUTPUT);
+    pinMode(buzzerPin, OUTPUT);
+    pinMode(motorPin, OUTPUT);
+    pinMode(MQ2pin, INPUT);
+    delay(20000); // allow the MQ2 to warm up
 }
 
-void loop() {
-	sensorValue = analogRead(MQ2pin); // read Gaz value from sensor
+void loop()
+{
+    sensorValue = analogRead(MQ2pin); // read Gaz value from sensor
 
-	Serial.print("MQ2 Sensor Value: ");
-	Serial.println(sensorValue);
+    Serial.print("MQ2 Sensor Value: ");
+    Serial.println(sensorValue);
 
-  if (sensorValue >= 360)	{
-    digitalWrite(redLedPin, HIGH);
-    digitalWrite(greenLedPin, LOW);
-    digitalWrite(buzzerPin, HIGH);
-    digitalWrite(motorPin, HIGH);
-  }
-  else {
-    digitalWrite(greenLedPin, HIGH);
-    digitalWrite(redLedPin, LOW);
-    digitalWrite(buzzerPin, LOW);
-    digitalWrite(motorPin, LOW);
-  }
-	delay(2000); // wait 2s for next reading
+    if (sensorValue >= 360)
+    {
+        digitalWrite(redLedPin, HIGH);
+        digitalWrite(greenLedPin, LOW);
+        digitalWrite(buzzerPin, HIGH);
+        digitalWrite(motorPin, HIGH);
+    }
+    else
+    {
+        digitalWrite(greenLedPin, HIGH);
+        digitalWrite(redLedPin, LOW);
+        digitalWrite(buzzerPin, LOW);
+        digitalWrite(motorPin, LOW);
+    }
+    delay(2000); // wait 2s for next reading
 }
